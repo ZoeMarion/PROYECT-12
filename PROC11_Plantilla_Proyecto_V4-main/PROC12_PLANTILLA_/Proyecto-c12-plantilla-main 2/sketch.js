80b3e3062ba9cbf84c235415a30dc9ebe6140a38
ZoeMarion <111403678+ZoeMarion@users.noreply.github.com>
2022-09-08 18:59:21 -0500

    Add files via upload

---
+++ PROC11_Plantilla_Proyecto_V4-main/PROC12_PLANTILLA_/Proyecto-c12-plantilla-main 2/sketch.js
@@ -0,0 +1,55 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+  //loadImage de path (camino)
+  pathImg = loadImage("ath.png");
+  //loadAnimation de boy (niño)
+  boyImg = loadAnimation("Runner-1.png", "Runner-2.png")
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+ 
+ path=createSprite(200,200);
+ path.addImage(pathImg);
+ path.velocityY = 4;
+ path.scale=1.2;
+
+//crear sprite de boy (niño)
+boy = createSprite(180,340,30,30);
+//agregar animación para boy
+boy.scale=0.08;
+boy.addAnimation("JakeRunning", boyImg);
+  
+// crear  left Boundary (límite izquierdo)
+leftBoundary=createSprite(0,0,100,800);
+////establecer visibilidad como false (falso) para límite izquierdo
+leftBoundary.visible = false;
+//crear right Boundary (límite derecho)
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
+
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  // boy moviéndose en el eje X con el mouse
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
+  
+  //código para reiniciar el fondo
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+  
+  drawSprites();
+}
